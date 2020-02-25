9f37bdf0190cec8a88cafbfa7d19891867ef7778
Thomas Argent <tfargent@gmail.com>
2020-02-24 19:51:25 -0500

    content

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry:{
+        app: "./public/index.js",
+        db: "./public/db.js"
+    },
+    output: {
+        path: __dirname + "/public/dist",
+        filename: "[name].bundle.js"
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+          cacheId: "my-domain-cache-id",
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: ".client/service-worker.js",
+          minify: true,
+          staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+        }),
+        new WebpackPwaManifest({
+          name: "Budget App",
+          short_name: "budget",
+          description: "An application that allows you to track your income and expenses.",
+          background_color: "#01579b",
+          theme_color: "#ffffff",
+          "theme-color": "#ffffff",
+          start_url: "/",
+          icons: [{
+            src: path.resolve("./public/assets/images/icons/android-chrome-192x192.png"),
+            sizes: [192, 512],
+            destination: path.join("assets", "icons")
+          }]
+        })
+      ]
+
+}
+module.exports = config;
